45e522235b6abead0f155502edf3c9726a6b7236
raidolContreras <oscarcontrerasf91@gmail.com>
2023-09-18 14:59:33 -0600

    Update plantilla.php

--- view/plantilla.php
+++ view/plantilla.php
@@ -78,8 +78,8 @@ session_start();
 	<script src="assets/js/app.min.js"></script>
 	<script src="assets/js/custom.js"></script>
 	<script src="assets/js/prism.js"></script>
-	<script src="../assets/libs/dataTable/datatables.min.js"></script>
-	<script src="../assets/libs/dataTable/datatables.js"></script>
+	<script src="app-fiscal/assets/libs/dataTable/datatables.min.js"></script>
+	<script src="app-fiscal/assets/libs/dataTable/datatables.js"></script>
 	<script src="assets/js/app/Cerrar_Sesion.js"></script>
 </body>
 
