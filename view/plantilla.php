6b2f4be756d04d7041cdc2e8a7ce4bea0499ee52
raidolContreras <oscarcontrerasf91@gmail.com>
2023-09-18 15:07:31 -0600

    Update plantilla.php

--- view/plantilla.php
+++ view/plantilla.php
@@ -13,6 +13,7 @@ session_start();
 	<link rel="stylesheet" href="assets/css/inStyle.css" />
 	<link href="assets/libs/datatable/datatables.min.css" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<script src="assets/libs/dataTable/datatables.min.js"></script>
  
 	<?php if (isset($_GET["pagina"])): ?>
 		<title><?php echo 'IN Fiscal - '.$_GET["pagina"]; ?></title>
@@ -76,9 +77,8 @@ session_start();
 
 	<script src="assets/js/sidebarmenu.js"></script>
 	<script src="assets/js/app.min.js"></script>
-		<script src="assets/js/custom.js"></script>
-		<script src="assets/js/prism.js"></script>
-	<script src="assets/libs/dataTable/datatables.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+	<script src="assets/js/prism.js"></script>
 	<script src="assets/js/app/Cerrar_Sesion.js"></script>
 </body>
 
