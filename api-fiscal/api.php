6252ae38718f37dff342a7e7546e45358603c6e4
raidolContreras <oscarcontrerasf91@gmail.com>
2023-10-27 15:05:58 -0600

    formato corregido

--- api-fiscal/api.php
+++ api-fiscal/api.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-Type: application/json");
 require_once 'controller/controller.php';
 // Ruta para obtener todos los títulos y sus capítulos, secciones, artículos y párrafos asociados.
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['resource']) && $_GET['resource'] === 'titles') {
