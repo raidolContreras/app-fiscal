695030fb20818e49899b89ce0d47a9255a2f7b62
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-08 14:09:09 -0600

    Update controller.php

--- api-fiscal/controller/controller.php
+++ api-fiscal/controller/controller.php
@@ -7,12 +7,12 @@ class ControllerApi{
 	static public function titles($item, $value){
 		$resultados = ModelsApi::titles($item, $value);
 	    if (!empty($resultados)) {
+	        
+			$datos = array(
+				"results" => array()
+			);
 			
 	        if ($item != null && $value != null) {
-
-			$datos = array(
-				"reglament" => array()
-			);
 		        foreach ($resultados as $fila) {
 		            $capituloId = intval($fila['idChapters']);
 		            $seccionId = intval($fila['idSections']);
@@ -25,16 +25,36 @@ class ControllerApi{
 		            $resultado['status_title'] = $fila['status_title'];
 		            $resultado['type_title'] = $fila['type_title'];
 		            $resultado['Admin_idAdmin'] = $fila['Admin_idAdmin'];
-		            $resultado['cover_name'] = $fila['cover_name'];
+
+		            if ($capituloId) {
+		                $resultado['capitulos'][$capituloId]['name_Chapter'] = $fila['name_Chapter'];
+
+		                if ($seccionId) {
+		                    $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['name_section'] = $fila['name_section'];
+
+		                    if ($articuloId) {
+		                        $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['articulos'][$articuloId]['name_article'] = $fila['name_article'];
+
+		                        if ($parrafoId) {
+		                            $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['articulos'][$articuloId]['parrafos'][$parrafoId] = array(
+		                                'paragraph' => $fila['paragraph'],
+		                                'position' => $fila['position']
+		                            );
+		                        }
+		                    }
+		                }
+			            }
+			        // Agrega la información de la portada.
+		            $resultado['cover'] = array(
+		                'idCover' => intval($fila['idCover']),
+		                'cover_name' => $fila['cover_name']
+		            );
 					
 		        }
-					$datos['reglament'][] = $resultado;
+					$datos['results'][] = $resultado;
 	            echo json_encode($datos, JSON_PRETTY_PRINT);
 		    } else{
 				
-				$datos = array(
-					"results" => array()
-				);
 				foreach ($resultados as $fila) {
 					// Agrega la información de la portada a la lista de resultados.
 					$resultado = array(
@@ -43,8 +63,7 @@ class ControllerApi{
 						"idCover" => intval($fila['idCover']),
 						"cover_name" => $fila['cover_name']
 					);
-
-					
+				
 					$datos['results'][] = $resultado;
 				}
 				
