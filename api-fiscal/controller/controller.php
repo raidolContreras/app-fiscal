748cb8ef2f1ae1d8b8da1b21b29029e0c624d009
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-08 15:02:09 -0600

    Update controller.php

--- api-fiscal/controller/controller.php
+++ api-fiscal/controller/controller.php
@@ -6,57 +6,80 @@ class ControllerApi{
 
 	static public function titles($item, $value){
 		$resultados = ModelsApi::titles($item, $value);
-	    if (!empty($resultados)) {
-	        
+		if (!empty($resultados)) {
+			
 			$datos = array(
-				"results" => array(
-					"capitulos" => array(
-					)
-				)
+				"results" => array()
 			);
 			
-	        if ($item != null && $value != null) {
-		        foreach ($resultados as $fila) {
-		            $capituloId = intval($fila['idChapters']);
-		            $seccionId = intval($fila['idSections']);
-		            $articuloId = intval($fila['idArticles']);
-		            $parrafoId = intval($fila['idParagraph']);
+			if ($item != null && $value != null) {
+				foreach ($resultados as $fila) {
+					$capituloId = intval($fila['idChapters']);
+					$seccionId = intval($fila['idSections']);
+					$articuloId = intval($fila['idArticles']);
+					$parrafoId = intval($fila['idParagraph']);
 
-		            // Agrupa los datos por título, capítulo, sección, artículo y párrafo.
-		            $resultado['idTitles'] = intval($fila['idTitles']);
-		            $resultado['name_title'] = $fila['name_title'];
-		            $resultado['status_title'] = $fila['status_title'];
-		            $resultado['type_title'] = $fila['type_title'];
-		            $resultado['Admin_idAdmin'] = $fila['Admin_idAdmin'];
+					// Agrupa los datos por título, capítulo, sección, artículo y párrafo.
+					$resultado = array(
+						"idTitles" => intval($fila['idTitles']),
+						"name_title" => $fila['name_title'],
+						"status_title" => $fila['status_title'],
+						"type_title" => $fila['type_title'],
+						"Admin_idAdmin" => $fila['Admin_idAdmin'],
+						"capitulos" => array()
+					);
 
-		            if ($capituloId) {
-		                $resultado['capitulos'][$capituloId]['name_Chapter'] = $fila['name_Chapter'];
+					if ($capituloId) {
+						$capitulo = array(
+							"idChapter" => $capituloId,
+							"name_Chapter" => $fila['name_Chapter'],
+							"secciones" => array()
+						);
 
-		                if ($seccionId) {
-		                    $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['name_section'] = $fila['name_section'];
+						if ($seccionId) {
+							$seccion = array(
+								"idSection" => $seccionId,
+								"name_section" => $fila['name_section'],
+								"articulos" => array()
+							);
 
-		                    if ($articuloId) {
-		                        $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['articulos'][$articuloId]['name_article'] = $fila['name_article'];
+							if ($articuloId) {
+								$articulo = array(
+									"idArticle" => $articuloId,
+									"name_article" => $fila['name_article'],
+									"parrafos" => array()
+								);
 
-		                        if ($parrafoId) {
-		                            $resultado['capitulos'][$capituloId]['secciones'][$seccionId]['articulos'][$articuloId]['parrafos'][$parrafoId] = array(
-		                                'paragraph' => $fila['paragraph'],
-		                                'position' => $fila['position']
-		                            );
-		                        }
-		                    }
-		                }
-			            }
-			        // Agrega la información de la portada.
-		            $resultado['cover'] = array(
-		                'idCover' => intval($fila['idCover']),
-		                'cover_name' => $fila['cover_name']
-		            );
-					
-		        }
+								if ($parrafoId) {
+									$parrafo = array(
+										"idParrafo" => $parrafoId,
+										"paragraph" => $fila['paragraph'],
+										"position" => $fila['position']
+									);
+									$articulo["parrafos"][] = $parrafo;
+								}
+
+								$seccion["articulos"][] = $articulo;
+							}
+
+							$capitulo["secciones"][] = $seccion;
+						}
+
+						$resultado["capitulos"][$capituloId] = $capitulo;
+					}
+
+					// Agrega la información de la portada.
+					$resultado['cover'] = array(
+						'idCover' => intval($fila['idCover']),
+						'cover_name' => $fila['cover_name']
+					);
+
 					$datos['results'][] = $resultado;
-	            echo json_encode($datos, JSON_PRETTY_PRINT);
-		    } else{
+				}
+
+				// Convierte el arreglo asociativo en JSON y muestra el resultado con formato.
+				echo json_encode($datos, JSON_PRETTY_PRINT);
+			} else{
 				
 				foreach ($resultados as $fila) {
 					// Agrega la información de la portada a la lista de resultados.
@@ -72,30 +95,30 @@ class ControllerApi{
 				
 				echo json_encode($datos, JSON_PRETTY_PRINT);
 				
-		    }
+			}
 		} else {
-            echo json_encode(array('mensaje' => 'No se encontraron registros de títulos.'), JSON_PRETTY_PRINT);
-	    }
+			echo json_encode(array('mensaje' => 'No se encontraron registros de títulos.'), JSON_PRETTY_PRINT);
+		}
 	}
 	static public function createUser($name, $email, $password) {
 
-        if (!isValidName($name)) {
-            return json_encode(['message' => 'El nombre no es válido']);
-        } elseif (!isValidEmail($email)) {
-            return json_encode(['message' => 'El correo electrónico no es válido']);
-        } elseif (!isValidPassword($password)) {
-            return json_encode(['message' => 'La contraseña no cumple con los requisitos']);
-        } else {
-            // Contraseña válida, encripta la contraseña con crypt
-            $salt = '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$';
-            $hashedPassword = crypt($password, $salt);
+		if (!isValidName($name)) {
+			return json_encode(['message' => 'El nombre no es válido']);
+		} elseif (!isValidEmail($email)) {
+			return json_encode(['message' => 'El correo electrónico no es válido']);
+		} elseif (!isValidPassword($password)) {
+			return json_encode(['message' => 'La contraseña no cumple con los requisitos']);
+		} else {
+			// Contraseña válida, encripta la contraseña con crypt
+			$salt = '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$';
+			$hashedPassword = crypt($password, $salt);
 
-            $registro = ModelsApi::createUser($name, $email, $hashedPassword);
+			$registro = ModelsApi::createUser($name, $email, $hashedPassword);
 
-            // Devuelve una respuesta JSON
-            return json_encode(['message' => $registro]);
-        }
-    }
+			// Devuelve una respuesta JSON
+			return json_encode(['message' => $registro]);
+		}
+	}
 
 	static public function loginUser($email, $password) {
 		$datos = array();
