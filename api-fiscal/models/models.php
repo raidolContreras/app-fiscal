a67adb33a3f02a57cdaaaff97b7a29335cf4bfb9
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-08 15:59:54 -0600

    Update models.php

--- api-fiscal/models/models.php
+++ api-fiscal/models/models.php
@@ -29,7 +29,7 @@ class ModelsApi{
                     FROM app_titles t
                     LEFT JOIN app_chapter c ON t.idTitles = c.Title_idTitles
                     LEFT JOIN app_sections s ON c.idChapters = s.Chapter_idChapters
-                    LEFT JOIN app_articles a ON (a.Title_idTitles = t.idTitles OR a.Chapter_idChapters = c.idChapters) AND (a.Section_idSections = s.idSections OR a.Section_idSections = 0)
+                    LEFT JOIN app_articles a ON (a.Chapter_idChapters = c.idChapters) AND (a.Section_idSections = s.idSections OR a.Section_idSections = 0)
                     LEFT JOIN app_paragraph p ON a.idArticles = p.articles_idArticles
                     LEFT JOIN app_covers cv ON t.idTitles = cv.Title_idTitles
                     WHERE t.type_title = :type_title AND t.idTitles = :idTitles";
