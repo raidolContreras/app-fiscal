59f9e8511a4a78f0cb265b63a81024022dce2479
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-09 10:24:33 -0600

    Update controller.php

--- api-fiscal/controller/controller.php
+++ api-fiscal/controller/controller.php
@@ -25,7 +25,7 @@ class ControllerApi{
 		            $resultado['Admin_idAdmin'] = intval($fila['Admin_idAdmin']);
 	                $resultado['idCover'] = intval($fila['idCover']);
 	                $resultado['cover_name'] = $fila['cover_name'];
-	                $resultado['chapters']
+	                $resultado['chapters'] = [];
 
 		            if ($capituloId && $fila['chapter_title'] == $fila['idTitles']) {
 		                $resultado['chapters'][$capituloId]['name_Chapter'] = $fila['name_Chapter'];
