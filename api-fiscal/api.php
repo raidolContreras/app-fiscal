ab3b555ed90b21ee59c627ba12f8d5c0131950e5
raidolContreras <oscarcontrerasf91@gmail.com>
2023-11-27 10:20:26 -0600

    Update api.php

--- api-fiscal/api.php
+++ api-fiscal/api.php
@@ -117,12 +117,13 @@ if (isset($_GET['resource'])) {
     $article = intval($_GET['searchArticle']);
     $seeArticle = ControllerApi::searchArticles($article, null);
 
+    $results['article'][] = array(
+        'idArticle' => intval($seeArticle[0]['idArticles']),
+        'name_article' => $seeArticle[0]['name_article'],
+        'paragraph' => array();
+    );
+
     foreach ($seeArticle as $result) {
-        $results['results'][] = array(
-            'idArticle' => intval($result['idArticles']),
-            'name_article' => $result['name_article'],
-            
-        );
     }
 
     print_r($results);
